fd503de035f685c367d030e6c808100272e3d712
acisdude <acisdude@cfa.harvard.edu>
2019-11-14 17:44:23 -0500

    Fixes

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 export SKA=/proj/sot/ska
-PATH=/data/acis/ska3/bin:${PATH}
-cd /data/acis/ska3_pkg/src/acis_viols_tracking
+PATH=/data/acis/ska/bin:${PATH}
+cd /data/acis/ska_pkg/acis_viols_tracking
 
 make track 2>&1 >/dev/null
 make html 2>&1 >/dev/null
